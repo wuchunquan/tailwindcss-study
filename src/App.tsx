9b03613f643915ef3c39916fd9b287ae49029885
wcq <744800102@qq.com>
2022-09-26 18:55:12 +0800

    add route

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import './App.css'
 import Home from "./pages/Home";
-import {createBrowserRouter, Routes, RouterProvider,Route, BrowserRouter} from "react-router-dom"
+import {Routes, Route, BrowserRouter} from "react-router-dom"
 import Header from "./components/Header/Header";
 import Docs from "./pages/Docs";
+import Examples from "./pages/Examples";
+import About from "./pages/About";
+import News from "./pages/News";
 
 // const router = createBrowserRouter([
 //     {path: "/", element: <Home/>},
@@ -17,6 +20,9 @@ function App() {
                 <Routes >
                     <Route path="/" element={<Home/>}/>
                     <Route path="/docs" element={<Docs/>}/>
+                    <Route path="/examples" element={<Examples/>}/>
+                    <Route path="/about" element={<About/>}/>
+                    <Route path="/news" element={<News/>}/>
                 </Routes>
 
             </div>
